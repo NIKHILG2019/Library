04a51864d5a54bb132f85b8df749a2dc96f1734c
NIKHILG2019 <gangaramaninikhil@gmail.com>
2020-03-15 22:05:53 +0530

    GUI PUSH

--- loginPage.py
+++ loginPage.py
@@ -1,10 +1,17 @@
 import tkinter as tk
+import DATABASE
+db = DATABASE.Database_connect
+db.Database_Connection('nikhil', '123')
 m = tk.Tk()
 m.title("Login Page")
 lable1 = tk.Label(m, text="username ")
 lable1.grid(row=0, column=0)
 text1 = tk.Text(m, width="15", height="1")
 text1.grid(row=0, column=1)
+lable2 = tk.Label(m, text="Password ")
+lable2.grid(row=1, column=0)
+text2 = tk.Text(m, width="15", height="1")
+text2.grid(row=1, column=1)
 button1 = tk.Button(m, text="Login", command=m.destroy, width="10", height="1")
-button1.grid(column=1, row=1)
+button1.grid(column=1, row=2)
 m.mainloop()
