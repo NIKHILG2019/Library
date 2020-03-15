77e29013a7312fea80c6b8bd73f44d320fbab162
NIKHILG2019 <gangaramaninikhil@gmail.com>
2020-03-15 22:39:28 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	loginPage.py

--- loginPage.py
+++ loginPage.py
@@ -1,7 +1,9 @@
 import tkinter as tk
 import DATABASE
-db = DATABASE.Database_connect
-db.Database_Connection('', '')
+def onClick() :
+    db = DATABASE.Database_connect
+    db.Database_Connection("nikhil", "123")
+    m.destroy()
 m = tk.Tk()
 m.title("Login Page")
 lable1 = tk.Label(m, text="username ")
@@ -12,6 +14,6 @@ lable2 = tk.Label(m, text="Password ")
 lable2.grid(row=1, column=0)
 text2 = tk.Text(m, width="15", height="1")
 text2.grid(row=1, column=1)
-button1 = tk.Button(m, text="Login", command=m.destroy, width="10", height="1")
+button1 = tk.Button(m, text="Login", command=onClick, width="10", height="1")
 button1.grid(column=1, row=2)
 m.mainloop()
