e78554b4877d619f3641e2ea1f65b742669c8d62
NIKHILG2019 <gangaramaninikhil@gmail.com>
2020-03-16 04:33:17 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	loginPage.py

--- dashboard.py
+++ dashboard.py
@@ -1,22 +1,24 @@
 import tkinter as tk
 import studentEntry as s
-def onClick1() :
-    objs = s.studententry
-    objs.ui(s.studententry)
-def onClick2() :
-    import bookEntry
-def onClick3() :
-    import bookIssue
-def onClick4() :
-    import bookReturn
-d = tk.Tk()
-d.title("DashBoard")
-button1 = tk.Button(d, width="10", height="1", text="Student Entry", command=onClick1)
-button1.grid(row=1, column=0)
-button2 = tk.Button(d, width="10", height="1", text="Book Entry", command=onClick2)
-button2.grid(row=1, column=1)
-button3 = tk.Button(d, width="10", height="1", text="Book issue", command=onClick3)
-button3.grid(row=1, column=2)
-button4 = tk.Button(d, width="10", height="1", text="Book Return", command=onClick4)
-button4.grid(row=1, column=3)
-d.mainloop()
+class dashboard :
+    def onClick1() :
+        objs = s.studententry
+        objs.ui(s.studententry)
+    def onClick2() :
+        import bookEntry
+    def onClick3() :
+        import bookIssue
+    def onClick4() :
+        import bookReturn
+    def ui(self):
+        d = tk.Tk()
+        d.title("DashBoard")
+        button1 = tk.Button(d, width="10", height="1", text="Student Entry", command=dashboard.onClick1)
+        button1.grid(row=1, column=0)
+        button2 = tk.Button(d, width="10", height="1", text="Book Entry", command=dashboard.onClick2)
+        button2.grid(row=1, column=1)
+        button3 = tk.Button(d, width="10", height="1", text="Book issue", command=dashboard.onClick3)
+        button3.grid(row=1, column=2)
+        button4 = tk.Button(d, width="10", height="1", text="Book Return", command=dashboard.onClick4)
+        button4.grid(row=1, column=3)
+        d.mainloop()
--- loginPage.py
+++ loginPage.py
@@ -1,19 +1,32 @@
 import tkinter as tk
-def onClick():
-    m.destroy()
-    import dashboard
-m = tk.Tk()
-m.title("Login Page")
-lable1 = tk.Label(m, text="username ")
-lable1.grid(row=0, column=0)
-Entry1 = tk.Entry(m, width="15")
-Entry1.grid(row=0, column=1)
-lable2 = tk.Label(m, text="Password ")
-lable2.grid(row=1, column=0)
-Entry2 = tk.Entry(m, width="15")
-Entry2.grid(row=1, column=1)
-button1 = tk.Button(m, text="Login", command=onClick, width="10", height="1")
-button1.grid(column=1, row=2)
-lable3 = tk.Label(m, text="")
-lable3.grid(row=2, column=0)
-m.mainloop()
+import dashboard
+import mysql.connector
+class loginpage :
+    def onClick(self, Entry1, Entry2, m,lable3):
+        username = Entry1.get()
+        password = Entry2.get()
+        try:
+            db = mysql.connector.connect(host="localhost", user=username, passwd=password, database="LIBRARY")
+            if db.is_connected() :
+                m.destroy()
+                objd = dashboard
+                objd.dashboard.ui(dashboard)
+        except:
+            lable3.configure(text="Not Connected with database")
+    def ui(self):
+        m = tk.Tk()
+        m.title("Login Page")
+        lable1 = tk.Label(m, text="username ")
+        lable1.grid(row=0, column=0)
+        Entry1 = tk.Entry(m, width="15")
+        Entry1.grid(row=0, column=1)
+        lable2 = tk.Label(m, text="Password ")
+        lable2.grid(row=1, column=0)
+        Entry2 = tk.Entry(m, width="15")
+        Entry2.grid(row=1, column=1)
+        button1 = tk.Button(m, text="Login", command=lambda: loginpage.onClick(loginpage, Entry1, Entry2, m, lable3), width="10", height="1")
+        button1.grid(column=1, row=2)
+        lable3 = tk.Label(m, text="")
+        lable3.grid(row=4, column=2)
+        m.mainloop()
+loginpage.ui(loginpage)
