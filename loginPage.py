e9c081920954de09ac089c71073fe7a37d7805ae
NIKHILG2019 <gangaramaninikhil@gmail.com>
2020-03-15 21:24:10 +0530

    GUI PUSH

--- loginPage.py
+++ loginPage.py
@@ -1,3 +1,8 @@
-import tkinter
-m = tkinter.Tk()
+import tkinter as tk
+m = tk.Tk()
+m.title("Login Page")
+text1 = tk.Text(m, width="10", height="2")
+text1.pack()
+button1 = tk.Button(m, text="Login", command=m.destroy, width="10", height="1")
+button1.pack()
 m.mainloop()
