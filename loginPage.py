6105a143b6704bc146f18ec924cdb0544a50aa00
NIKHILG2019 <gangaramaninikhil@gmail.com>
2020-03-16 08:08:13 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	loginPage.py

--- loginPage.py
+++ loginPage.py
@@ -4,16 +4,19 @@ import globalVar
 import dashboard
 class loginpage :
     def onClick(self, Entry1, Entry2, m,lable3):
-        username = Entry1.get()
-        password = Entry2.get()
-        try:
-                globalVar.db1 = mysql.connector.connect(host="localhost", user=username, passwd=password, database="LIBRARY")
-                if globalVar.db1.is_connected():
-                    m.destroy()
-                    objd = dashboard
-                    objd.dashboard.ui(dashboard)
-        except :
-            lable3.configure(text="Invalid Username or Password")
+        if Entry1.get()=='' or  Entry2.get()=='' :
+            lable3.configure(text="Username or Password cannot be empty")
+        else :
+            username = Entry1.get()
+            password = Entry2.get()
+            try:
+                    globalVar.db1 = mysql.connector.connect(host="localhost", user=username, passwd=password, database="LIBRARY")
+                    if globalVar.db1.is_connected():
+                        m.destroy()
+                        objd = dashboard
+                        objd.dashboard.ui(dashboard)
+            except :
+                lable3.configure(text="Invalid Username or Password")
     def ui(self):
         m = tk.Tk()
         m.geometry('600x150+550+300')
