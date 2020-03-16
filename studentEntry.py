6ff2789c84765ec560f767b1f44171660df18827
NIKHILG2019 <gangaramaninikhil@gmail.com>
2020-03-16 09:23:44 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	loginPage.py

--- studentEntry.py
+++ studentEntry.py
@@ -1,15 +1,14 @@
 import tkinter as tk
 import globalVar
 class studententry:
-    def onClick(self, id_no, name, se, lable3):
-        if id_no.get()=='' or name.get()=='':
+    def onClick(self, name, se, lable3):
+        if name.get()=='':
             lable3.configure(text = "Entries Cannot be Empty ")
         else :
-            id = id_no.get()
             name1 = name.get()
             c = globalVar.db1.cursor()
-            sql = "INSERT INTO STUDENT (idNo, name) VALUES (%s, %s)"
-            val = (id, name1)
+            sql = "INSERT INTO STUDENT (NAME) VALUES(%s)"
+            val = (name1,)
             try:
                 c.execute(sql, val)
                 globalVar.db1.commit()
@@ -20,16 +19,12 @@ class studententry:
     def ui(self):
         se = tk.Tk()
         se.geometry('600x150+550+300')
-        Entry1 = tk.Entry(se, width="15")
-        Entry1.grid(row=0, column=1)
         Entry2 = tk.Entry(se, width="15")
         Entry2.grid(row=1, column=1)
         se.title("Student Entry")
-        lable1 = tk.Label(se, text="ID")
-        lable1.grid(row=0, column=0)
         lable2 = tk.Label(se, text="name")
         lable2.grid(row=1, column=0)
-        button1 = tk.Button(se, text="Save", command=lambda:studententry.onClick(self, Entry1, Entry2, se, lable3), width="10", height="1")
+        button1 = tk.Button(se, text="Save", command=lambda:studententry.onClick(self, Entry2, se, lable3), width="10", height="1")
         button1.grid(column=0, row=2)
         lable3 = tk.Label(se, text="")
         lable3.grid(row=4, column=2)
