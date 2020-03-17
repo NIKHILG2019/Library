6a91a531f0d094b633593813fcd92f63224f7ebd
NIKHILG2019 <gangaramaninikhil@gmail.com>
2020-03-18 04:55:40 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	loginPage.py

--- bookReturn.py
+++ bookReturn.py
@@ -2,8 +2,12 @@ import tkinter as tk
 import globalVar
 class bookreturn :
     def onclick( self, Entry1, br, lable3, row1, column1):
-        if Entry1.get() == '':
-            lable3.configure(text = "Entry cannot be empty .")
+        lable3.configure(text="")
+        if Entry1.get() == '' or not Entry1.get().isdigit() :
+            if Entry1.get()=='':
+                lable3.configure(text = "Entry cannot be empty .")
+            else :
+                lable3.configure(text="Invalid ID")
         else :
             idNo=Entry1.get()
             sql="SELECT * FROM issue WHERE idNo=%s"
@@ -12,20 +16,33 @@ class bookreturn :
             c.execute(sql, val)
             res = c.fetchall()
             if len(res)!=0:
+                lable = []
                 ++column1
                 for i in res :
+                    lable3.configure(text="")
                     row = i
                     row1 = row1+1
                     lablen = tk.Label(br, text="idNo : "+str(row[0])+" "+"Name : "+row[1]+" "+"bookId : "+str(row[2])+" "+"bookName : "+row[3]+" "+"IssueDate : "+str(row[4]))
                     lablen.grid(row=row1, column=column1)
+                    lable.append(lablen)
                 lable4 = tk.Label(br, text="Enter bookId ")
                 lable4.grid(row=1, column=0)
                 Entry2 = tk.Entry(br, width="15")
                 Entry2.grid(row=1, column=1)
                 button2 = tk.Button(br, text="Return", command=lambda : bookreturn.onclick1(bookreturn, Entry2, res, br, lable3), width="10", height="1")
                 button2.grid(row=1, column=2)
+            else :
+                lable3.configure(text="Student not found.")
     def onclick1( self, Entry2, res, br, lable3 ):
-        print("Returned")
+        lable3.configure(text="")
+        if Entry2 ==''or not Entry2.get().isdigit():
+            if Entry2.get()=='':
+                lable3.configure(text="This field Cannot be Empty : ")
+            else :
+                lable3.configure(text="Invalid BookID")
+        else :
+            bookId=Entry2.get()
+
     def ui( self ):
         br = tk.Tk()
         br.geometry('1000x350+350+300')
